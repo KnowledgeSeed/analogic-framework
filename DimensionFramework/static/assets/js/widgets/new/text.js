a08302f9545198d4d432923e7c1dcef3f98c2c60
Oravecz Tamás <tomi.oravecz@gmail.com>
2021-04-15 19:50:23 +0200

    ctrl v ok, context menu

--- DimensionFramework/static/assets/js/widgets/new/text.js
+++ DimensionFramework/static/assets/js/widgets/new/text.js
@@ -56,8 +56,91 @@ class TextWidget extends Widget {
         }
     }
 
+    static getEditables(gridId) {
+        return $('#' + gridId).find('.ks-text-title[data-editable=1]').filter(':visible').sort(function (a, b) {
+            return ($(b).data('ordinal')) < ($(a).data('ordinal')) ? 1 : -1;
+        });
+    }
+
+    static pasteData(text, editables, j, f, o, section) {
+        let e, rows = text.trim().split('\n'), cells = [], i, k, s, r;
+        if (rows.length === 0) {
+            return;
+        }
+
+        let editableRows = TextWidget.createEditableRows(editables, j);
+        for (i = 0; i < editableRows.length; ++i) {
+            if (rows.length <= i) {
+                break;
+            }
+            cells = rows[i].split('\t');
+            for (k = 0; k < editableRows[i].length; ++k) {
+                if (k >= cells.length) {
+                    break;
+                }
+                e = $(editableRows[i][k]);
+                s = e.closest('section').attr('id');
+                WidgetValue[s] = {value: Utils.escapeText(cells[k])};
+                r = $('<div>').data('id', s).data('action', 'write').data('ordinal', e.data('ordinal'));
+                Widget.doHandleSystemEvent(r, f);
+                e.html(cells[k]);
+            }
+        }
+        TextWidget.addEdit(section, o, true);
+    }
+
+    static createEditableRows(editables, j) {
+        let result = [], rowIndex = -1, sgi, row = [], a;
+        while (j < editables.length) {
+            sgi = $(editables[j]).closest('section').attr('id').split('_');
+            a = parseInt(sgi[1]);
+            if ((rowIndex !== a || j === editables.length - 1) && rowIndex !== -1) {
+                if (j === editables.length - 1) {
+                    row.push(editables[j]);
+                }
+                result.push(row);
+                row = [];
+            }
+            row.push(editables[j]);
+            rowIndex = a;
+            ++j;
+        }
+        return result;
+    }
+
+    static getCurrentIndex(editables, c) {
+        let i = 0, j = -1;
+        while (i < editables.length && j === -1) {
+            if ($(editables[i]).data('ordinal') === c.data('ordinal')) {
+                j = i;
+            }
+            ++i;
+        }
+        if ((j + 1) >= editables.length) {
+            j = -1;
+        }
+        return j;
+    }
+
     static addEdit(section, o, amIOnGridTable) {
-        section.find('.ks-text-title').on('click', e => {
+        let textTitle = section.find('.ks-text-title');
+        if (amIOnGridTable === true) {
+            textTitle.bind('contextmenu', e => {
+                let r = $('<div>').data('id', section.attr('id')).data('action', 'rightclick').data('ordinal', textTitle.data('ordinal'));
+                Widget.doHandleSystemEvent(r, e);
+                Widget.doHandleGridTableSystemEvent(r, e);
+                return false;
+            });
+            /* textTitle.on('mousedown', e => {
+                 if (e.which === 3) {
+                     Utils.stopEvent(e);
+                     let r = $('<div>').data('id', section.attr('id')).data('action', 'rightclick').data('ordinal', textTitle.data('ordinal'));
+                     Widget.doHandleSystemEvent(r, e);
+                     return false;
+                 }
+             });*/
+        }
+        textTitle.on('click', e => {
             let c = $(e.currentTarget);
             c.off('click');
 
@@ -76,33 +159,32 @@ class TextWidget extends Widget {
                 if (amIOnGridTable === true) {
                     let gridId = r.data('id').split('_')[0];
                     r.on('keydown', f => {
-
                         if (f.keyCode === 39 || f.keyCode === 37) {
-                            let editables = $('#' + gridId).find('.ks-text-title[data-editable=1]').filter(':visible').sort(function (a, b) {
-                                return ($(b).data('ordinal')) < ($(a).data('ordinal')) ? 1 : -1;
-                            }), i = 0, j = -1, k = 0;
-                            while (i < editables.length && j === -1) {
-                                if ($(editables[i]).data('ordinal') === c.data('ordinal')) {
-                                    j = i;
-                                }
-                                ++i;
-                            }
-                            if ((j + 1) >= editables.length) {
-                                j = -1;
-                            }
-                            k = f.keyCode === 39 ? j + 1 : j === -1 ? editables.length - 1 : j - 1;console.log(k);
+                            let editables = TextWidget.getEditables(gridId),
+                                j = TextWidget.getCurrentIndex(editables, c), k = 0;
+
+                            k = f.keyCode === 39 ? j + 1 : j === -1 ? editables.length - 1 : j - 1;
 
                             $(editables[k]).click();
                         }
 
-                        if(f.keyCode === 38 || f.keyCode === 40) {
-                            let sgi = r.data('id').split('_'), gridId = sgi[0], actRow = parseInt(sgi[1]), row = f.keyCode === 38 ? actRow === 0 ? -1 : actRow - 1 : actRow + 1, column = sgi[2];
-                            if(row === -1){
+                        if (f.keyCode === 38 || f.keyCode === 40) {
+                            let sgi = r.data('id').split('_'), gridId = sgi[0], actRow = parseInt(sgi[1]),
+                                row = f.keyCode === 38 ? actRow === 0 ? -1 : actRow - 1 : actRow + 1, column = sgi[2];
+                            if (row === -1) {
                                 return;
                             }
                             let nextElement = $('#' + gridId + '_' + row + '_' + column);
-                            if(nextElement.length && nextElement.is(':visible')){
+                            if (nextElement.length && nextElement.is(':visible')) {
                                 nextElement.find('.ks-text-title').click();
+                            }
+                        }
+
+                        if (f.ctrlKey && f.keyCode === 86) {
+                            let editables = TextWidget.getEditables(gridId),
+                                j = TextWidget.getCurrentIndex(editables, c);
+                            if (j >= 0 && editables.length > 0) {
+                                navigator.clipboard.readText().then(text => TextWidget.pasteData(text, editables, j, f, o, section)).catch(err => L('Read from clipboard failed: ', err));
                             }
                         }
                     });
