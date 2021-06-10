7ae380775e5027d04cc7a97992cb91d0be6f342b
modosd <modosd97@gmail.com>
2021-06-10 10:46:28 +0200

    Rep bug

--- DimensionFramework/static/assets/js/configs/rochebpsp/repository.js
+++ DimensionFramework/static/assets/js/configs/rochebpsp/repository.js
@@ -152,7 +152,7 @@ app.repository = {
                 query: [
                     (r, x) => {
                         return {
-                            title: r.Cells[x].FormattedValue + '<br/><div style=\"font-size:10px; float: left;margin-right: 1%;margin-top:0.5%;";  >' + r.Cells[x+2].FormattedValue + '</div>' + '<div style=\"font-size:10px;color:#408CD9;;margin-top:0.5%;\" >' + r.Cells[x+3].FormattedValue + '</div>',
+                            title: r.Cells[x].FormattedValue + '<br/><div style=\"font-size:10px; float: left;margin-right: 1%;margin-top:0.5%;";  >' + r.Cells[x + 2].FormattedValue + '</div>' + '<div style=\"font-size:10px;color:#408CD9;;margin-top:0.5%;\" >' + r.Cells[x + 3].FormattedValue + '</div>',
                             body: r.Cells[x + 1].FormattedValue
                         }
                     }]
