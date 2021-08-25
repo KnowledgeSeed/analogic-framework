b64e628e877a7563b7020dfaba8d234746bde479
Oravecz Tamás <tomi.oravecz@gmail.com>
2021-08-25 23:35:16 +0200

    módosítások

---
+++ DimensionFramework/Customer/Hays/__init__.py
@@ -0,0 +1,3 @@
+from DimensionFramework.Customer.Hays import Export
+from DimensionFramework.Customer.Hays import Validation
+from DimensionFramework.Customer.Hays import BusinessLogic
---
+++ DimensionFramework/Customer/Roche/__init__.py
@@ -0,0 +1,3 @@
+from DimensionFramework.Customer.Roche import Export
+from DimensionFramework.Customer.Roche import Validation
+from DimensionFramework.Customer.Roche import BusinessLogic
