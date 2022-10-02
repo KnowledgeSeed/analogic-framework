b95af730fc80f7110819b74f7c1d434524bed8f2
akos.nagy <nagy.akos.andras@gmail.com>
2022-10-02 22:49:24 +0200

    Adding cypress support

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    supportFile: 'support/e2e.js',
+    supportFile: false,
     specPattern: 'e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
