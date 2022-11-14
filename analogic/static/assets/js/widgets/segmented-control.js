61178b2b4b346929dd27547b7e607bdaae6b3fa4
Oravecz Tamás <tomi.oravecz@gmail.com>
2022-11-14 23:27:05 +0100

    allow to put segmented-contol on gridtable

--- analogic/static/assets/js/widgets/segmented-control.js
+++ analogic/static/assets/js/widgets/segmented-control.js
@@ -1,6 +1,7 @@
 /* global app, Listeners, QB, Widget */
 
 'use strict';
+
 class SegmentedControlWidget extends Widget {
 
     getHtml(widgets, d) {
@@ -9,24 +10,55 @@ class SegmentedControlWidget extends Widget {
         };
         const o = this.options;
 
-        let vv = d ? d : o.widgets.map(e => {return {value: e.value, selected: e.selected, label: e.label}});
+        let vv = d ? d : o.widgets.map(e => {
+            return {value: e.value, selected: e.selected, label: e.label}
+        });
 
         let selected = false, i;
-        for(i = 0; i < vv.length; ++i){
-            if(vv[i].selected){
+        for (i = 0; i < vv.length; ++i) {
+            if (vv[i].selected) {
                 selected = {selected: vv[i].label, value: vv[i].value};
             }
         }
-        if(selected === false && vv.length > 0){
+        if (selected === false && vv.length > 0) {
             selected = {selected: vv[0].label, value: vv[0].value};
         }
 
-        if(selected) {
+        if (selected) {
             this.selected = selected.selected;
             this.value = selected.value;
         }
 
         return `<div class="ks-segmented ks-segmented-${v.skin}"  style="${this.getGeneralStyles(d).join('')}"><div class="ks-segmented-inner">${widgets.join('')}</div></div>`;
+    }
+
+    embeddedRender(withState, data, loadFunction = QB.loadData) {
+        const o = {...this.options, ...data}, instance = this, h = Listeners.handle;
+
+        let widgetOptions, widgets = [], i = 0, childrenData, w, widgetHtmls = [];
+
+        for (widgetOptions of o.widgets || []) {
+            widgets.push(this.getWidget(widgetOptions));
+        }
+
+        for (w of widgets) {
+            childrenData = {id: o.id, position: i, originalId: w.id};
+            widgetHtmls.push(w.embeddedRender(withState, {...childrenData, ...data[i]}));
+
+            ++i;
+        }
+
+        this.addListeners(false);
+
+        this.addDependents();
+
+        if (new o.type(o).amIOnAGridTable()) { //Todo check
+            Listeners.push({options: o, method: 'refreshGridCell', eventName: 'forcerefresh.' + o.id, handler: h});
+        }
+
+        let visible = data && typeof data.visible !== 'undefined' ? data.visible : o.visible;
+
+        return instance.getMainHtmlElement(o, data, visible, widgetHtmls, withState)
     }
 
     render(withState) {
@@ -41,7 +73,8 @@ class SegmentedControlWidget extends Widget {
         this.addListeners(false);
 
         return QB.loadData(o.id, instance.name).then(function (data) {
-            let deferred = [], w, i = 0, childrenData, widgetData = Array.isArray(data) ? data : (data && data.data) ? data.data : [];
+            let deferred = [], w, i = 0, childrenData,
+                widgetData = Array.isArray(data) ? data : (data && data.data) ? data.data : [];
 
             for (w of widgets) {
                 childrenData = {id: o.id, position: i, originalId: w.id};
@@ -58,7 +91,7 @@ class SegmentedControlWidget extends Widget {
                 }
 
                 let visible = data && typeof data.visible !== 'undefined' ? data.visible : o.visible;
-                return `<section title="${o.title || ''}" ${visible === false ? 'style="display:none"' : 'style="display:contents;"' } id="${o.id}">${instance.getHtml(widgetHtmls, instance.processData(data), withState)}</section>`;
+                return `<section title="${o.title || ''}" ${visible === false ? 'style="display:none"' : 'style="display:contents;"'} id="${o.id}">${instance.getHtml(widgetHtmls, instance.processData(data), withState)}</section>`;
             });
         });
     }
@@ -77,10 +110,15 @@ class SegmentedControlWidget extends Widget {
 
             Widget.doHandleSystemEvent(s, e, false);
             Widget.doHandleSystemEvent(second, e, false);
+            if (this.amIOnAGridTable()) {
+                Widget.doHandleGridTableSystemEvent(s, e);
+                Widget.doHandleGridTableSystemEvent(second, e);
+            }
         });
 
         section.find('a').on('click', e => {
-            let s = $(e.target).closest('section').parent().closest('section').find('.ks-segment'), b = false, i, w = $(e.target).closest('a');
+            let s = $(e.target).closest('section').parent().closest('section').find('.ks-segment'), b = false, i,
+                w = $(e.target).closest('a');
 
             $(e.target).closest('section').parent().closest('section').find('.ks-segment').removeClass('ks-on').removeClass('ks-right').removeClass('ks-left');
 
