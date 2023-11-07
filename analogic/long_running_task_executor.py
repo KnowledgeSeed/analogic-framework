03741975f6c455f02fcf082236c02f17bd431536
Oravecz Tamás <tomi.oravecz@gmail.com>
2023-11-07 09:52:07 +0100

    long running task inteface modifications

--- analogic/long_running_task_executor.py
+++ analogic/long_running_task_executor.py
@@ -6,7 +6,11 @@ class LongRunningTaskExecutor(ABC):
         pass
 
     @abstractmethod
-    def cancel_long_running_request(self, **kwargs):
+    def on_long_running_request_canceled(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def on_long_running_request_completed(self, **kwargs):
         pass
 
     @abstractmethod
