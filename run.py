0dd4f9f58613fedc0c82a696cb58977906d58da1
Oravecz Tamás <tomi.oravecz@gmail.com>
2024-07-01 22:38:30 +0200

    run.py waitress

--- run.py
+++ run.py
@@ -26,5 +26,8 @@ if __name__ == "__main__":
         from gevent.pywsgi import WSGIServer
         http_server = WSGIServer(('0.0.0.0', 5000), app)
         http_server.serve_forever()
+    elif os.environ.get('ANALOGIC_WSGI_SERVER') == 'waitress':
+        from waitress import serve
+        serve(app, host='0.0.0.0', port=5000)
     else:
         app.run(host='0.0.0.0', port='5000')
